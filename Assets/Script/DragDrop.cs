771cf84a6b68a9e60d4ac7dfb345254db6fe9536
4d3Lx4d0 <129032805+4d3Lx4d0@users.noreply.github.com>
2025-03-17 21:16:13 +0700

    Drag drop mechanic

---
+++ Assets/Script/DragDrop.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    public GameObject dropLocation, stickerLocation;
+    private RectTransform rectTransform;
+    private GameObject duplicate;
+    private Canvas canvas;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        // Debug.Log("Pointer Down on " + gameObject.name);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // Debug.Log("Drag Started");
+
+        duplicate = Instantiate(gameObject, transform.parent);
+        duplicate.transform.SetParent(canvas.transform, true);
+        duplicate.transform.SetAsLastSibling();
+        duplicate.GetComponent<DragDrop>().enabled = true;
+        duplicate.name = gameObject.name;
+        rectTransform = duplicate.GetComponent<RectTransform>();
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvas.transform as RectTransform, eventData.position, eventData.pressEventCamera, out Vector2 localPoint
+        );
+        rectTransform.localPosition = localPoint;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvas.transform as RectTransform, eventData.position, eventData.pressEventCamera, out Vector2 localPoint
+        );
+        rectTransform.localPosition = localPoint;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // Debug.Log("Drag Ended");
+
+        if (IsDroppedOnTarget(eventData))
+        {
+            // Debug.Log("Dropped on correct location!");
+            duplicate.transform.SetParent(dropLocation.transform, false);
+            duplicate.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+            //duplicate.SetActive(false);
+
+        }
+        else
+        {
+            Destroy(duplicate);
+        }
+    }
+
+    private bool IsDroppedOnTarget(PointerEventData eventData)
+    {
+        RectTransform dropRect = dropLocation.GetComponent<RectTransform>();
+        return RectTransformUtility.RectangleContainsScreenPoint(dropRect, eventData.position, eventData.pressEventCamera);
+    }
+}
