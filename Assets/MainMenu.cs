426a3e7e8bc50a4321b4eae5968eb35e793f9eeb
4d3Lx4d0 <129032805+4d3Lx4d0@users.noreply.github.com>
2025-04-22 11:41:36 +0700

    main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void PlayGame()
+    {
+        SceneManager.LoadScene("1");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void goToSettings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+    public void goToMainmenu()
+    {
+        SceneManager.LoadScene("Main Menyu");
+    }
+    }
